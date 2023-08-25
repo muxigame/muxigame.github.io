854c6a59167287e016268f7dfc5f070889053348
muxigame <76636667+muxigame@users.noreply.github.com>
2023-08-26 04:37:40 +0800

    update url

---
+++ deplay_shadowsocks/command.sh
@@ -0,0 +1,1 @@
+bash <(curl -L https://muxigame.github.io/deploy_shadowsocks/shell.sh)
--- deplay_shadowsocks/shell.sh
+++ deplay_shadowsocks/shell.sh
@@ -1,6 +1,6 @@
 apt-get install -y curl
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
-wget https://muxigame.github.com/deploy_shadowsocks/config.json  -O -> /usr/local/etc/v2ray/config.json
+wget https://muxigame.github.io/deploy_shadowsocks/config.json  -O -> /usr/local/etc/v2ray/config.json
 ufw enable
 ufw allow 10086
 ufw reload
