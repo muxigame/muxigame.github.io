e1b4b3a178c231163559f6cf7922bf71f0726db4
roc <roc.shen@outlook.com>
2024-10-09 22:44:52 +0800

    use 443 for cloudflare

--- deploy_shadowsocks/install_node.sh
+++ deploy_shadowsocks/install_node.sh
@@ -3,5 +3,5 @@ apt-get install -y curl
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 wget https://muxigame.github.io/deploy_shadowsocks/config.json  -O -> /usr/local/etc/v2ray/config.json
 ufw enable
-ufw allow 10086
+ufw allow 443
 ufw reload
