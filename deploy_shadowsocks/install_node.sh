37fb7741aa83fe6dd05d888a121f2171b1aa0c5f
muxigame <76636667+muxigame@users.noreply.github.com>
2025-03-15 16:53:11 +0800

    Update install_node.sh

--- deploy_shadowsocks/install_node.sh
+++ deploy_shadowsocks/install_node.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 apt-get install -y curl
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
-wget https://muxigame.github.io/deploy_shadowsocks/config.json  -O -> /usr/local/etc/v2ray/config.json
+wget https://muxigame.github.io/deploy_shadowsocks/install_config.json  -O -> /usr/local/etc/v2ray/config.json
 ufw enable
 ufw allow 443
 ufw reload
